6a09f738a3d4270e94d4746ee751bb42545b61ba
Caio Rodrigues <caio.rodrigues.oliveira@ee.ufcg.edu.br>
2021-02-14 18:42:37 -0300

    Potentiometer Reading

---
+++ Class Examples/(5) Potentiometer Reading/potentiometer_reading.ino
@@ -0,0 +1,44 @@
+#define PotPin       A0
+#define LowBand      11
+#define SuitableBand 10
+#define HighBand      9
+
+float PotVoltage = 0; 
+float PotMxResistance = 10; // Potentiometer maximum resistance in Kilo-Ohms
+float PotResistance;
+int Delay = 200;
+
+void setup(){
+	Serial.begin(9600);
+ 	pinMode(PotPin, INPUT);
+  	pinMode(LowBand, OUTPUT);
+    pinMode(SuitableBand, OUTPUT);
+    pinMode(HighBand, OUTPUT);
+}
+
+void loop(){
+  
+	PotVoltage = analogRead(PotPin);
+    PotResistance = PotMxResistance - ((10.0/1023.0) * PotVoltage);
+  	Serial.print("Potentiometer Resistance: ");
+  	Serial.print(PotResistance);
+ 	Serial.println("Kilo-Ohms");
+  	
+  	if(PotResistance >= 9){
+      digitalWrite(HighBand, HIGH);
+  	  digitalWrite(LowBand, LOW);
+      digitalWrite(SuitableBand, LOW);
+    }
+ 	else if(PotResistance <= 1){
+      digitalWrite(HighBand, LOW);
+  	  digitalWrite(LowBand, HIGH);
+      digitalWrite(SuitableBand, LOW);
+    }
+    else{
+      digitalWrite(HighBand, LOW);
+  	  digitalWrite(LowBand, LOW);
+      digitalWrite(SuitableBand, HIGH);
+    }
+  	
+  	delay(Delay);
+}	
